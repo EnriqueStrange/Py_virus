6fa1866a841e12960af439c0cadc94660907406b
Mr.Strange <39236761+EnriqueStrange@users.noreply.github.com>
2022-03-12 13:03:35 +0530

    Add files via upload

---
+++ prankPy.py
@@ -0,0 +1,29 @@
+#Mr.Strange
+#PrankPy
+
+from tkinter import *
+from random import randint
+import time
+import threading
+
+root = Tk()
+root.attributes("-alpha", 0)
+root.overrideredirect(1)
+root.attributes("-topmost", 1)
+
+def place_windows():
+    while True:
+        win = Toplevel(root)
+        win.geometry("300x60+" + str(randint(0, root.winfo_screenwidth() - 300)) + "+" + str(randint(0, root.winfo_screenheight() - 60)))
+        win.overrideredirect(1)
+        Label(win, text="You got hacked", fg="red").place(relx=.38, rely=.3)
+        win.lift()
+        win.attributes("-topmost", True)
+        win.attributes("-topmost", False)
+        root.lift()
+        root.attributes("-topmost", True)
+        root.attributes("-topmost", False)
+        time.sleep(.05)
+
+threading.Thread(target=place_windows).start()
+root.mainloop()
